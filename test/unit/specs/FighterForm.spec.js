66a78ce4901464cacf8792d5a82fd2996d79af5b
Gene Huh <ehuh@teamexos.com>
2018-01-14 21:38:11 -0800

    add getter for selected fighter to fighterform test spec

--- test/unit/specs/FighterForm.spec.js
+++ test/unit/specs/FighterForm.spec.js
@@ -15,7 +15,21 @@ describe('FighterForm.vue', () => {
             addFighter: sinon.stub()
         }
         getters = {
-            selectedFighter: () => {},
+            selectedFighter: () => {
+                return {
+                    firstName: 'Max',
+                    lastName: 'Holloway',
+                    weight: '155',
+                    description: 'All-around striker with good grappling',
+                    groundGame: 7,
+                    boxing: 9,
+                    rangeStriking: 8,
+                    wrestling: 6,
+                    clinch: 9,
+                    id: 7,
+                    fighterQuotient: 7.8
+                }
+            },
             weightClasses: () => ['Heavy', '205', '185', '170', '155', '145', '135', '125', '115']
         }
         store = new Vuex.Store({
